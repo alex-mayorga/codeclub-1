cf6a7329c9f829c4cd4f81622313d83b6c4b428a
alex_mayorga <alex_mayorga@yahoo.com>
2024-04-26 23:48:18 -0500

    1st assignment

--- src/index.js
+++ src/index.js
@@ -6,10 +6,35 @@
  * - Run `npm run deploy` to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
- */
+ 
 
 export default {
 	async fetch(request, env, ctx) {
 		return new Response('Hello World!');
 	},
 };
+*/
+
+// src/index.js
+var src_default = {
+	async fetch(request, env, ctx) {
+		if(request.method == "POST") {
+			return new Response('POST method was used', {
+				headers: {
+					'content-type': 'application/json',
+				},
+			});
+		}
+		else{
+			return new Response('POST method expected', {
+				headers: {
+					'content-type': 'application/json',
+				},
+			});
+		}
+	}
+};
+export {
+	src_default as default
+};
+//# sourceMappingURL=index.js.map
